773749854c3f48094abd1318babaff607981661c
Jake Kelly <Jake Kelly>
2016-10-31 23:18:43 -0400

    Created a login system that cyrrently leads to nothing

--- scripts.js
+++ scripts.js
@@ -1,6 +1,83 @@
 
-function DisplayHeader() {
 
-    
+function signUpError(error) {
+
+    var errorMes = "*" +error;
+    document.getElementById("dispSignUpError").innerHTML = errorMes;
 
 }
+function signInError(error) {
+
+    var errorMes = "*" +error;
+    document.getElementById("dispSignInError").innerHTML = errorMes;
+
+}
+
+function signUp() {
+
+    var createUsername = document.getElementById("createUsername").value;
+    var email = document.getElementById("email").value;
+    var createPassword = document.getElementById("CreatePassword").value;
+    var confirmPassword = document.getElementById("ConfirmPassword").value;
+    var OK = 1;
+
+    if (createUsername == '' || email == '' || createPassword == '' || confirmPassword == '') {
+        signUpError("All Fields Are Not Completed");
+        var OK = 0;
+
+    }
+
+    if (confirmPassword !== createPassword){
+        signUpError("Passwords Do Not Match");
+        var OK = 0;
+    }
+
+    if (OK == 1){
+
+        var Username = createUsername;
+        var Password = createPassword;
+        localStorage.setItem(Username, Username);
+        localStorage.setItem(Password, Password);
+
+        window.location.href = "Sign_In.html";
+
+    }
+}
+
+function signIn() {
+
+    var usernameLogin = document.getElementById("username").value;
+    var passwordLogin = document.getElementById("password").value;
+
+    var TesterUser = localStorage.getItem(usernameLogin);
+    var TesterPass = localStorage.getItem(passwordLogin);
+
+    if (usernameLogin == 'Hidden' && passwordLogin == 'Reset') {
+
+        localStorage.clear();
+        alert("Reset")
+    }
+
+    if (usernameLogin == '' || passwordLogin == ''){
+        signInError("All Fields Are Not Completed");
+    }
+
+    if(TesterUser == usernameLogin && TesterPass == passwordLogin){
+        alert("Login")
+
+    } else {
+        signInError("Username or Password is Incorrect");
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
